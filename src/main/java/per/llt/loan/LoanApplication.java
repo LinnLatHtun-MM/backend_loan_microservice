4057fcc4b979c27a8dbc68485971a55cbf30f0c3
linnlathtun <linnlathtun99.mm@gmail.com>
2025-03-26 15:07:50 +0700

    Create SpringBoot Java Loan Project

---
+++ src/main/java/per/llt/loan/LoanApplication.java
@@ -0,0 +1,13 @@
+package per.llt.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanApplication.class, args);
+    }
+
+}
